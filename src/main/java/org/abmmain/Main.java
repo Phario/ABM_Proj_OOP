3a1d70f51fd2ff6e4ad5676fb6b91e4008198a25
Phario <grzedyn@gmail.com>
2024-06-07 03:32:49 +0200

    "Minor changes"

--- src/main/java/org/abmmain/Main.java
+++ src/main/java/org/abmmain/Main.java
@@ -2,22 +2,16 @@
 
 import org.critters.*;
 import org.maps.*;
-
 import java.util.ArrayList;
 
-public class Main {
-    public int hungerDrain = 5;
-    //make variables for simulation configuration
-    public static void main(String[] args) {
-        ArrayList<ACritter> listOfBears = new ArrayList<>();
-        listOfBears.add(new Bear("Bear", 0.5, 0, 0));
-        System.out.println(listOfBears.get(0).getSpecies());
-    }
+import static org.maps.Config.*;
 
-    public static class Berries {
-        protected int amount;
-        public Berries(int amount) {
-            this.amount = amount;
+ public class Main {
+     public static ACritter[][] map;
+    public static void main(String[] args) {
+        config();
+        for (int i = 0; i < turnAmount; i++) {
+           Map.turnManager(map);
         }
     }
 }
--- src/main/java/org/critters/ACritter.java
+++ src/main/java/org/critters/ACritter.java
@@ -79,11 +79,12 @@ public abstract class ACritter implements Critters {
         return globalCritterID;
     }
 
-    // Getters for position
+    // Getters and setters for position
     public int getX() {
         return x;
     }
-
+    public void setX(int x) {this.x = x;}
+    public void setY(int y) {this.y = y;}
     public int getY() {
         return y;
     }
